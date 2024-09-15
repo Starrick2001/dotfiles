d9beee6d6627140d7faef98a03784e2798658080
Starrick2001 <lebuidihoa257@gmail.com>
2024-09-16 00:04:51 +0700

    update nvchad

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -9,17 +9,22 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python"
+      },
+    },
+  },
 }
