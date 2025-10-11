80562119e7813a4662c02bcb11dced9545a078c1
Starrick2001 <lebuidihoa257@gmail.com>
2025-10-11 17:51:16 +0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+ln -s ~/dotfiles/nvim/ ~/.config/nvim
+ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
