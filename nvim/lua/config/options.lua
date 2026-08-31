61071cebe64a45637c2959dd305930f419eb9d4a
Starrick2001 <lebuidihoa257@gmail.com>
2026-08-31 16:49:17 +0700

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 -- set the following line in your config/options.lua
 vim.g.lazyvim_blink_main = true
 -- Set to false to disable auto format
-vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_eslint_auto_format = false
 
 vim.g.lazyvim_picker = "snacks"
 
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,17 +1,49 @@
 return {
   "stevearc/conform.nvim",
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_format = "never" })
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
+    },
+  },
   opts = function()
     require("conform").setup({
+      notify_on_error = true,
+      stop_after_first = true,
       formatters_by_ft = {
         javascript = { "biome", "prettier", stop_after_first = true },
         typescript = { "biome", "prettier", stop_after_first = true },
+        vtsls = { "biome", "prettier", stop_after_first = true },
       },
       formatters = {
         biome = {
           require_cwd = true, -- Only runs if biome.json is found in the project root
         },
+        prettier = {
+          cwd = require("conform.util").root_file({ ".prettierrc", "package.json" }),
+        },
       },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      default_format_opts = {
+        -- lsp_format = "fallback",
+      },
+      format_on_save = function(bufnr)
+        local ignore_filetypes = { "sql", "yaml", "yml" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
   end,
 }
